6c37a764b571f73c1dd6dac4bc5360093c651f6d
Dan Gohman <dev@sunfishcode.online>
2023-02-08 06:23:10 -0800

    Experimental Preview2 support.

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,27 @@
-//! Raw API bindings to the WebAssembly System Interface (WASI)
-//!
-//! This crate provides Rust API bindings to WASI APIs. All WASI APIs are
-//! exported from this crate and provided with the appropriate type signatures.
-//! This crate is entirely procedurally generated from the `*.witx` files that
-//! describe the WASI API.
-//!
-//! # WASI API Version
-//!
-//! The WASI API is evolving over time. It is both gaining new features as well
-//! as tweaking the ABI of existing features. As a result it's important to
-//! understand what version of this crate you're using and how it relates to
-//! the WASI version of the spec.
-//!
-//! The WASI specification is organized into phases where there is a snapshot
-//! at any one point in time describing the current state of the specification.
-//! This crate implements a particular snapshot. You can find the snapshot
-//! version implemented in this crate in the build metadata of the crate
-//! version number. For example something like `0.9.0+wasi-snapshot-preview1`
-//! means that this crate's own personal version is 0.9.0 and it implements the
-//! `wasi-snapshot-preview1` snapshot. A major release of this crate (i.e.
-//! bumping the "0.9.0") is expected whenever the generated code changes
-//! or a new WASI snapshot is used.
-//!
-//! # Crate Features
-//!
-//! This crate supports one feature, `std`, which implements the standard
-//! `Error` trait for the exported [`Error`] type in this crate. This is
-//! enabled by default but can be disabled to make the library `no_std`
-//! compatible.
+//! API bindings to the WebAssembly System Interface (WASI)
 
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
-mod lib_generated;
-pub use lib_generated::*;
+// Generate the bindings.
 
-/// Special `Dircookie` value indicating the start of a directory.
-pub const DIRCOOKIE_START: Dircookie = 0;
+// There are a few options for the generated bindings. One option we may
+// want to use in the future is `unchecked`; this option disables the
+// generated code that checks that interface values are well-formed. At
+// this time, enough things are experimental that it's worth keeping the
+// checks on, but in the future it may be a desirable optimization.
+//
+// Another option is that instead of using these proc macros, we could
+// generate the bindings ahead-of-time using the wit-bindgen CLI. That's
+// a little less convenient for development but saves compile time and
+// build complexity for users.
 
-/// The "standard input" descriptor number.
-pub const FD_STDIN: Fd = 0;
+#[cfg(feature = "std")]
+wit_bindgen_guest_rust::generate!({
+    world: "experiment",
+});
 
-/// The "standard output" descriptor number.
-pub const FD_STDOUT: Fd = 1;
-
-/// The "standard error" descriptor number.
-pub const FD_STDERR: Fd = 2;
+#[cfg(not(feature = "std"))]
+wit_bindgen_guest_rust::generate!({
+    world: "experiment",
+    no_std,
+});
